0430c63bbd0563c4da25a13ec57e0a718f4a51e2
agutierrezmorag <alvarogutierrezmoraga@gmail.com>
2024-03-27 01:18:07 -0300

    Refactor data display and add histogram functionality

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import pandas as pd
 import streamlit as st
 
@@ -9,50 +10,65 @@ def load_and_display_data(title, filename, qualitative_vars):
     data = pd.read_csv(f"datasets/{filename}")
     st.dataframe(data, hide_index=True, use_container_width=True)
     col1, col2 = st.columns(2)
-    with col1:
-        st.markdown("### Variables cualitativas")
-        qualitative = qualitative_stats(data, qualitative_vars)
-        st.dataframe(qualitative, hide_index=True, use_container_width=True)
-    with col2:
-        st.markdown("### Variables cuantitativas")
-        data_descriptors = descriptors(data)
-        st.dataframe(data_descriptors, hide_index=False, use_container_width=True)
+
+    st.markdown("### Variables cualitativas")
+    qualitative = qualitative_stats(data, qualitative_vars)
+    st.dataframe(qualitative, hide_index=True, use_container_width=True)
+
+    st.markdown("### Variables cuantitativas")
+    data_descriptors = descriptors(data)
+    st.dataframe(data_descriptors, hide_index=False, use_container_width=True)
+
+    # Add a selectbox for the user to select a quantitative variable
+    quantitative_vars = [col for col in data.columns if col not in qualitative_vars]
+    selected_var = st.selectbox(
+        "Selecciona una variable para generar un histograma", quantitative_vars
+    )
+
+    # Plot a histogram of the selected variable
+    fig, ax = plt.subplots()
+    data[selected_var].hist(ax=ax)
+    ax.set_title(f"Histograma de {selected_var}")
+    st.pyplot(fig)
 
 
 def main():
-    st.set_page_config(page_title="Datamart data", page_icon="📊", layout="wide")
-    st.title("📊 Datamart data")
-    st.markdown(
-        "Los siguientes datos no han sido modificados y se presentan tal como se encuentran en el archivo `csv`."
-    )
+    st.set_page_config(page_title="Datamart data", page_icon="📊")
 
-    st.info(
-        "📢 **Nota:** Considerar que se usa un punto en vez de una coma para valores decimales."
-    )
+    # Define the datasets and their corresponding titles and qualitative variables
+    datasets = {
+        "🏅 Olympics data": (
+            "olympics.csv",
+            ["Team", "NOC", "Games", "Sport", "Event", "Medal"],
+        ),
+        "📚 Schooling data": ("expected-years-of-schooling.csv", ["Entity", "Code"]),
+        "🪙 Income data": ("gross-national-income-per-capita.csv", ["Entity", "Code"]),
+        "🌍 Human Development Index (HDI) data": (
+            "human-development-index.csv",
+            ["Entity", "Code"],
+        ),
+        "📈 Historical Index of Human Development (HIHD) data": (
+            "hdi-vs-hihd.csv",
+            ["Entity", "Code"],
+        ),
+    }
 
-    load_and_display_data(
-        "🏅 Olympics data",
-        "olympics.csv",
-        ["Team", "NOC", "Games", "Sport", "Event", "Medal"],
-    )
-    load_and_display_data(
-        "📚 Schooling data",
-        "expected-years-of-schooling.csv",
-        ["Entity", "Code"],
-    )
-    load_and_display_data(
-        "🪙 Income data", "gross-national-income-per-capita.csv", ["Entity", "Code"]
-    )
-    load_and_display_data(
-        "🌍 Human Development Index (HDI) data",
-        "human-development-index.csv",
-        ["Entity", "Code"],
-    )
-    load_and_display_data(
-        "📈 Historical Index of Human Development (HIHD) data",
-        "hdi-vs-hihd.csv",
-        ["Entity", "Code"],
-    )
+    with st.sidebar:
+        st.markdown("# 📊 Datamart data")
+        st.markdown(
+            "Los siguientes datos no han sido modificados y se presentan tal como se encuentran en el archivo `csv`."
+        )
+
+        st.info(
+            "📢 **Nota:** Considerar que se usa un punto en vez de una coma para valores decimales."
+        )
+        # Add a selectbox for the user to select a dataset
+        selected_dataset = st.selectbox("Elige un dataset", list(datasets.keys()))
+
+        # Load and display the selected dataset
+        filename, qualitative_vars = datasets[selected_dataset]
+
+    load_and_display_data(selected_dataset, filename, qualitative_vars)
 
 
 if __name__ == "__main__":
