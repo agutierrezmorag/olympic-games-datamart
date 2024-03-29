22b33e36b9821181b18c6af8eb508e7ee90857d7
agutierrezmorag <alvarogutierrezmoraga@gmail.com>
2024-03-29 18:17:46 -0300

    improved chain of thought display

--- pages/ai.py
+++ pages/ai.py
@@ -3,7 +3,11 @@ import os
 import pandas as pd
 import streamlit as st
 from langchain.agents.agent_types import AgentType
+from langchain.memory import ConversationBufferWindowMemory
 from langchain_anthropic import ChatAnthropic
+from langchain_community.chat_message_histories.streamlit import (
+    StreamlitChatMessageHistory,
+)
 from langchain_experimental.agents.agent_toolkits import create_pandas_dataframe_agent
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_openai import ChatOpenAI
@@ -133,6 +137,19 @@ def main():
         st.error("No valid dataset selected.")
         st.stop()
 
+    if "msgs" not in st.session_state:
+        st.session_state.msgs = StreamlitChatMessageHistory(key="msgs")
+    if "memory" not in st.session_state:
+        st.session_state.memory = ConversationBufferWindowMemory(
+            k=5,
+            chat_memory=st.session_state.msgs,
+            return_messages=True,
+        )
+
+    # Historial de mensajes
+    for msg in st.session_state.msgs.messages:
+        st.chat_message(msg.type).write(msg.content)
+
     if query := st.chat_input("Ask me a question!"):
         st.chat_message("user", avatar="🤓").write(query)
         with st.chat_message("assistant"):
@@ -143,20 +160,34 @@ def main():
                 with st.expander("🧠 Show train of thought"):
                     for step in response["intermediate_steps"]:
                         if isinstance(llm, ChatOpenAI):
-                            st.markdown(f"🛠️ **Action:** {step[0].tool}")
-                            st.write(
-                                f"📥 **Action Input:** {step[0].tool_input['query']}"
-                            )
-                            st.write("✨ **Result:**")
-                            st.dataframe(step[1])
+                            st.markdown("#### 🛠️ **Action**")
+                            st.markdown(f"`{step[0].tool}`")
+
+                            st.markdown("#### 📥 **Action Input**")
+                            st.code(step[0].tool_input["query"])
+
+                            st.write("#### ✨ **Result**")
+                            if isinstance(step[1], pd.core.series.Series):
+                                st.dataframe(step[1])
+                            else:
+                                st.write(step[1])
                         else:
+                            st.markdown("#### 💡 **Thought**")
                             st.write(
-                                f"💡 **Thought:** {step[0].log.split('Thought: ')[1].split('Action: ')[0]}"
+                                step[0].log.split("Thought: ")[1].split("Action: ")[0]
                             )
-                            st.markdown(f"🛠️ **Action:** {step[0].tool}")
-                            st.write(f"📥 **Action Input:** {step[0].tool_input}")
-                            st.write("✨ **Result:**")
-                            st.dataframe(step[1])
+
+                            st.markdown("#### 🛠️ **Action**")
+                            st.markdown(f"`{step[0].tool}`")
+
+                            st.write("#### 📥 **Action Input**")
+                            st.code(step[0].tool_input)
+
+                            st.write("#### ✨ **Result**")
+                            if isinstance(step[1], pd.core.series.Series):
+                                st.dataframe(step[1])
+                            else:
+                                st.write(step[1])
             except Exception as e:
                 st.error(
                     f"An error occurred: {e}. Try again or reload the page by pressing `R`."
